84aab5a92429e135daf144075fba1b6f41b6bc78
Yuri Cardozo <103226844+Netunosz@users.noreply.github.com>
2023-10-01 21:10:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("abrir").onclick=()=>{
+    document.getElementById("menu").style.left="0px";
+}
+
+document.getElementById("fechar").onclick=()=>{
+    document.getElementById("menu").style.left="-200px";
+}
